9d8538a71b1bfc9dcc4f1b30ffa04e7b8cb92911
Castos Clarice <cclarice@un-d4.kzn.21-school.ru>
2021-05-14 08:28:21 +0300

    second

--- test.sh
+++ test.sh
@@ -1,80 +1,51 @@
 #!/bin/bash
 make re
 ((C=0))
-while [ $C -ne 16 ]
+((N=256))
+while [ $C -ne $N ]
 do
-	ARR[$C]="$(($RANDOM%16+1))"
-	((A=0))
-	((D=0))
-	while [ $A -lt $C ]
-	do
-		if [ ${ARR[$C]} -eq ${ARR[$A]} ]
-		then
-			((D=1))
-		fi
-		((A=$A+1))
-	done
-	if [ $D -eq 0 ]
-	then
-		((C=$C+1))
-	fi
-	echo ${ARR[*]}
+	ARR[$C]=$(($C))
+	((C++))
+done
+((P=1))
+while [ $P -ne 0 ]
+do
+((C=0))
+while [ $C -ne $N ]
+do
+	((R=$(($RANDOM%N))))
+	((B=${ARR[$R]}))
+	((ARR[$R]=${ARR[$C]}))
+	((ARR[$C]=$B))
+	((C++))
+done
+((P--))
 done
 ALL="${ARR[*]}"
+echo ${ARR[*]}
 ./push_swap $ALL
 
 
-
-
-
-
-
-
-
-
-
-
-
-#
-#	BERY SLOW METHOD
-#
-#((C=1))
-#echo Genarating Numbers
-#while [ $C == 1 ]
+#while [ $C -ne $N ]
 #do
+#	ARR[$C]=$(($RANDOM%$M+1))
 #	((A=0))
-#	((B=0))
-#	((C=0))
-#	ARR[0]="$(($RANDOM%16))"
-#	ARR[1]="$(($RANDOM%16))"
-#	ARR[2]="$(($RANDOM%16))"
-#	ARR[3]="$(($RANDOM%16))"
-#	ARR[4]="$(($RANDOM%16))"
-#	ARR[5]="$(($RANDOM%16))"
-#	ARR[6]="$(($RANDOM%16))"
-#	ARR[7]="$(($RANDOM%16))"
-#	ARR[8]="$(($RANDOM%16))"
-#	ARR[9]="$(($RANDOM%16))"
-#	ARR[10]="$(($RANDOM%16))"
-#	ARR[11]="$(($RANDOM%16))"
-#	ARR[12]="$(($RANDOM%16))"
-#	ARR[13]="$(($RANDOM%16))"
-#	ARR[14]="$(($RANDOM%16))"
-#	ARR[15]="$(($RANDOM%16))"
-#	while [ $A -lt 16 ]
+#	((D=0))
+#	while [ $A -lt $C ]
 #	do
-#		while [ $B -lt 16 ]
-#		do
-#			if [ ${ARR[$A]} -eq ${ARR[$B]} ]
-#			then
-#				if [ $A -ne $B ]
-#				then
-#					((C=1))
-#				fi
-#			fi
-#			((B=$B+1))
-#		done
-#		((A=$A+1))
-#		((B=0))
+#		if [ ${ARR[$A]} -eq $M ]
+#		then
+#			((M--))
+#		fi
+#		if [ ${ARR[$C]} -eq ${ARR[$A]} ]
+#		then
+#			echo "\033[1A[ ${#ARR[*]} | $M | $N ]"
+#			((D=1))
+#		fi
+#		((A++))
 #	done
+#	if [ $D -eq 0 ]
+#	then
+#		((C++))
+#	fi
 #done
